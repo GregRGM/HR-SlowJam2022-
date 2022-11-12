c581d9b70c96e47393854afd9ae0443745bb0e6c
Spencer Gellner <spencergellner@gmail.com>
2022-11-11 21:17:20 -0800

    [Added] Movement Pattern Enemy
    Enemy will move in a set pattern based on a list of positions. This will use the same firing considerations as the stationary for both forward and player.

--- Assets/Scripts/EnemyAttacks/MovementPatternEnemy.cs
+++ Assets/Scripts/EnemyAttacks/MovementPatternEnemy.cs
@@ -2,17 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementPatternEnemy : MonoBehaviour
+public class MovementPatternEnemy : BaseEnemyController
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TargetingType _targetingStyle;
+    private GameObject _player;
+    [SerializeField] private Transform _WeaponOrigin;
+
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private List<Transform> _movementPattern = new List<Transform>();
+
+    public int _patternIndex;
+    public float howclose;
+    private float timePast;
+    private bool _Increasing = true;
+
+    private void OnEnable()
     {
-        
+        _player = FindObjectOfType<PlayerFiringPlatform>().gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        time += Time.deltaTime;
+        bool timeToAttack = time >= _AttackDelay;
+        bool isCloseEnough = Vector3.Distance(transform.position, _movementPattern[_patternIndex].position) < howclose;
+        if (isCloseEnough)
+        {
+            GetNextPoint();
+        }
+        LerpToNextPoint();
+
+        if (timeToAttack)
+        {
+            Attack();
+        }
+
+    }
+
+    public override void Attack()
+    {
+        time = 0f;
+        if (_targetingStyle == TargetingType.ATPLAYER)
+        {
+            Vector3 AimDirection = (_player.transform.position - _WeaponOrigin.position).normalized;
+            Quaternion Directiontowards = Quaternion.LookRotation(AimDirection, Vector3.up);
+            _WeaponOrigin.rotation = Directiontowards;
+            _attackType.FireAttack();
+        }
+        else
+        {
+            _attackType.FireAttack();
+        }
+    }
+
+    private void LerpToNextPoint()
+    {
+        if (_patternIndex <= _movementPattern.Count -1 && _patternIndex >=0)
+        {
+            timePast += Time.deltaTime;
+            float ratioComplete = timePast / _moveSpeed;
+            transform.position = Vector3.Lerp(transform.position, _movementPattern[_patternIndex].position, ratioComplete);
+        }
+    }
+
+    private void GetNextPoint()
+    {
+        if (_patternIndex < _movementPattern.Count-1 && _Increasing)
+        {
+            _patternIndex++;
+            return;
+        }
+        else if (_patternIndex >= _movementPattern.Count-1)
+        {
+            _patternIndex--;
+            _Increasing = false;
+            timePast = 0;
+            return;
+            //return _movementPattern[_patternIndex].transform;
+        }
+        else if (!_Increasing && _patternIndex <= 0)
+        {
+            _Increasing = true;
+            _patternIndex++;
+            timePast = 0;
+            return;
+            //return _movementPattern[_patternIndex].transform;
+        }
+        else
+        {
+            _patternIndex--;
+            return;
+            //return _movementPattern[_patternIndex].transform;
+        }
     }
 }
