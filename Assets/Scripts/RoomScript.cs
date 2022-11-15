b1bb51b8819780d2b01510918665b131afe4c749
Spencer Gellner <spencergellner@gmail.com>
2022-11-15 00:18:55 -0800

    Created different option for camera switching

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RoomScript : MonoBehaviour
 {
@@ -14,50 +15,61 @@ public class RoomScript : MonoBehaviour
     public FreezeAxisType axisToFreeze;
     public Vector3 facing;
 
+    public CinemachineVirtualCamera RoomCamera;
+
     private void OnEnable()
     {
         _Player = FindObjectOfType<PlayerFiringPlatform>().gameObject;
+        CameraRoomSwitcher.Register(RoomCamera);
+    }
+    private void OnDisable()
+    {
+        CameraRoomSwitcher.UnRegister(RoomCamera);
     }
     [ContextMenu("Start Room")]
     public void StartRoom()
     {
         roomWaveHandler.gameObject.SetActive(true);
         roomWaveHandler.SetOwner(this);
+        CameraRoomSwitcher.SwitchCamera(RoomCamera);
+
         //_Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
 
-        _Player.GetComponent<Animator>().Play("Combat");
-        _Player.GetComponent<PlayerCameraControl>().SetCameraDetails(cameraPosition, screenLookAt);
+        //_Player.GetComponent<Animator>().Play("Combat");
+        // _Player.GetComponent<PlayerCameraControl>().SetCameraDetails(cameraPosition, screenLookAt);
         if (axisToFreeze == FreezeAxisType.X)
         {
-            _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
-            _Player.transform.rotation = Quaternion.LookRotation(facing);
+            _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation; 
         }
         if (axisToFreeze == FreezeAxisType.Y)
         {
             _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
-            _Player.transform.rotation = Quaternion.LookRotation(facing);
         }
         if (axisToFreeze == FreezeAxisType.Z)
         {
-            _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
-            _Player.transform.rotation = Quaternion.LookRotation(facing);
+            _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation; 
         }
+        _Player.transform.rotation = Quaternion.LookRotation(RoomCamera.transform.forward);
     }
     public void EndRoom()
     {
         roomWaveHandler.gameObject.SetActive(false);
         _Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-
-        _Player.GetComponent<Animator>().Play("DungeonTravel3D");
+        _Player.GetComponent<PlayerCameraFollow>().FollowPlayer();
+       // _Player.GetComponent<Animator>().Play("DungeonTravel3D");
         Debug.Log("Ended Room you can explore it now");
     }
 
     //Upon collision with another GameObject, this GameObject will reverse direction
     private void OnTriggerEnter(Collider other)
     {
-        if (!roomWaveHandler.IsDone) {
-            StartRoom();
-        }
+        if (other.gameObject.tag == "Player")
+        {
+            if (!roomWaveHandler.IsDone)
+            {
+                StartRoom();
+            }
+        } 
     }
 }
 
