41858d873276eb78f12bd3038bf89aede4e901aa
GregRGM <gregmorton.rgm@gmail.com>
2022-11-18 09:54:53 -0800

    Slightly updated the player's circle burst shot

--- Assets/Scripts/Weapons/BaseProjectile.cs
+++ Assets/Scripts/Weapons/BaseProjectile.cs
@@ -35,7 +35,7 @@ public class BaseProjectile : MonoBehaviour
         yield return new WaitForSeconds(_lifeTime);
         if (_isPooled)
         {
-            Debug.Log("I stopped bitch");
+            //Debug.Log("I stopped bitch");
             _proRB.velocity = Vector3.zero;
             StopAllCoroutines();
             ProjectilePoolManager.instance.ReturnPlayerSingleProjectile(this.gameObject);
