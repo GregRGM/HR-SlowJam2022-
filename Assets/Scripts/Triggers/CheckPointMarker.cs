3a24e5a70c787c1c8c6e1efb7a177ebbb7c65ed2
Spencer Gellner <spencergellner@gmail.com>
2022-11-18 22:46:02 -0800

    Added more work to the level platforms

--- Assets/Scripts/Triggers/CheckPointMarker.cs
+++ Assets/Scripts/Triggers/CheckPointMarker.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class CheckPointMarker : MonoBehaviour
 {
-    
+    [SerializeField] private CheckPointManager manager;
+
+    private void Start()
+    {
+        manager.RegisterCheckPoint(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            manager.UpdateCheckpoint(this);
+        }
+        
+    }
 }
