8920012d2a613fa7f3e70da136735a4eaa3d0160
Spencer Gellner <spencergellner@gmail.com>
2022-11-20 10:42:05 -0800

    Pushing build for jam

--- Assets/Menu Assets/MainMenuController.cs
+++ Assets/Menu Assets/MainMenuController.cs
@@ -6,17 +6,16 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject ControlsTab;
-    public GameObject HowToPlayTab;
+   // public GameObject HowToPlayTab;
     public GameObject ButtonTab;
     public GameObject ExitButton;
-
     public GameObject CreditsPanel;
     //tbd
     public GameObject MoreGamesPanel;
 
     private void Start()
     {
-        HowToPlayTab.SetActive(true);
+        //HowToPlayTab.SetActive(true);
         ControlsTab.SetActive(true);
         ButtonTab.SetActive(true);
     }
@@ -28,18 +27,27 @@ public class MainMenuController : MonoBehaviour
     public void DisplayCredits()
     {
         CreditsPanel.SetActive(true);
+        ExitButton.SetActive(true);
         CloseTabs();
     }
     private void CloseTabs()
     {
-        HowToPlayTab.SetActive(false);
+       // HowToPlayTab.SetActive(false);
         ControlsTab.SetActive(false);
         ButtonTab.SetActive(false);
     }
     public void CloseTab()
     {
         CreditsPanel.SetActive(false);
-        HowToPlayTab.SetActive(true);
+        //HowToPlayTab.SetActive(true);
+        ControlsTab.SetActive(true);
+        ButtonTab.SetActive(true);
+    }
+
+    public void DisplayNormal()
+    {
+        CreditsPanel.SetActive(false);
+        ExitButton.SetActive(false);
         ControlsTab.SetActive(true);
         ButtonTab.SetActive(true);
     }
--- Assets/Scripts/Triggers/EndGameTrigger.cs
+++ Assets/Scripts/Triggers/EndGameTrigger.cs
@@ -8,6 +8,10 @@ public class EndGameTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         bool isPlayer = other.gameObject.layer == 6;
+        if (isPlayer)
+        {
+            EnterEndMode();
+        }
     }
 
     public void EnterEndMode()
