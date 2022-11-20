bf8654e9d8a43c5f7d57c8c7b44b59091b3f0d08
Revnolion <evanjohnson155@gmail.com>
2022-11-19 20:33:06 -0800

    Added a plugin for textures and continued adding assets to the main level

---
+++ Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Unity Objects/Outputs/SubstanceOutputTexture.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Adobe.Substance
+{
+    [System.Serializable]
+    public class SubstanceOutputTexture
+    {
+        [SerializeField]
+        public int Index;
+
+        [SerializeField]
+        public int VirtualOutputIndex;
+
+        [SerializeField]
+        public SubstanceOutputDescription Description;
+
+        [SerializeField]
+        public Texture2D OutputTexture;
+
+        [SerializeField]
+        public bool sRGB;
+
+        [SerializeField]
+        public bool IsVirtual;
+
+        [SerializeField]
+        public bool IsStandardOutput;
+
+        [SerializeField]
+        public bool IsAlphaAssignable;
+
+        [SerializeField]
+        public string AlphaChannel;
+
+        [SerializeField]
+        public bool InvertAssignedAlpha;
+
+        [SerializeField]
+        public uint Flags = 0;
+
+        public SubstanceOutputTexture(SubstanceOutputDescription description, bool isStandard)
+        {
+            Index = description.Index;
+            Description = description;
+            IsStandardOutput = isStandard;
+
+            if (!string.IsNullOrEmpty(description.Channel))
+                IsAlphaAssignable = !string.Equals(description.Channel, "normal", System.StringComparison.OrdinalIgnoreCase);
+            else
+                IsAlphaAssignable = false;
+
+            IsVirtual = false;
+            sRGB = false;
+            OutputTexture = null;
+            AlphaChannel = string.Empty;
+        }
+    }
+}
---
+++ Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
@@ -0,0 +1,9 @@
+namespace Adobe.Substance
+{
+    public static class Version
+    {
+        public const string PluginVersion = "3.4.0-1-g259cbb5";
+        public const string EngineVersion = "8.2.0 0138644d";
+        public const int ImporterVersion = 340;
+    }
+}
